61dd97651ce42881795dc9f7c8c732ab9e75ff77
Szymon <szymon.wojcik@etu.hesge.ch>
2026-06-15 16:09:04 +0200

    feat: add deposit, withdraw and transfer functionality

--- src/main/java/com/szymonwojcik/wallet_app/controller/AccountController.java
+++ src/main/java/com/szymonwojcik/wallet_app/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.szymonwojcik.wallet_app.model.Account;
 import com.szymonwojcik.wallet_app.service.AccountService;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -17,17 +18,41 @@ public class AccountController {
     }
 
     @PostMapping
-    public Account create(@RequestBody Account account){
+    public Account create(@RequestBody Account account) {
         return accountService.create(account);
     }
 
     @GetMapping
-    public List<Account> getAll(){
+    public List<Account> getAll() {
         return accountService.getAll();
     }
 
     @GetMapping("/{id}")
-    public Account getById(@PathVariable Long id){
+    public Account getById(@PathVariable Long id) {
         return accountService.getById(id);
+    }
+
+    @PostMapping("/{id}/deposit")
+    public Account deposit(
+            @PathVariable Long id,
+            @RequestParam BigDecimal amount) {
+        return accountService.deposit(id, amount);
+    }
+
+    @PostMapping("/{id}/withdraw")
+    public Account withdraw(
+            @PathVariable Long id,
+            @RequestParam BigDecimal amount) {
+        return accountService.withdraw(id, amount);
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(
+                         @RequestParam Long from,
+                         @RequestParam Long to,
+                         @RequestParam BigDecimal amount) {
+        accountService.transfer(from, to, amount);
+
+        return "Transfer succesful";
     }
 }
--- src/main/java/com/szymonwojcik/wallet_app/service/AccountService.java
+++ src/main/java/com/szymonwojcik/wallet_app/service/AccountService.java
@@ -3,6 +3,7 @@ package com.szymonwojcik.wallet_app.service;
 import com.szymonwojcik.wallet_app.model.Account;
 import com.szymonwojcik.wallet_app.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,6 +12,42 @@ import java.util.List;
 public class AccountService {
 
     private final AccountRepository accountRepository;
+
+    public Account deposit(Long accountId, BigDecimal amount) {
+        Account account = getById(accountId);
+
+        account.setBalance(account.getBalance().add(amount));
+
+        return accountRepository.save(account);
+
+    }
+
+    public Account withdraw(Long accountId, BigDecimal amount) {
+        Account account = getById(accountId);
+
+        checkIfEnoughFunds(account, amount);
+
+        account.setBalance(account.getBalance().subtract(amount));
+
+        return accountRepository.save(account);
+    }
+
+    @Transactional
+    public void transfer(Long fromAccountId,
+                         Long toAccountId,
+                         BigDecimal amount) {
+        Account from = getById(fromAccountId);
+        Account to = getById(toAccountId);
+
+        checkIfEnoughFunds(from, amount);
+
+        from.setBalance(from.getBalance().subtract(amount));
+        to.setBalance(to.getBalance().add(amount));
+
+        accountRepository.save(from);
+        accountRepository.save(to);
+
+    }
 
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
@@ -26,6 +63,13 @@ public class AccountService {
     }
 
     public Account getById(Long id){
-        return  accountRepository.findById(id).orElseThrow();
+        return  accountRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Account not found"));
+    }
+
+    private void checkIfEnoughFunds(Account acc, BigDecimal am){
+        if(acc.getBalance().compareTo(am)<0) {
+            throw new RuntimeException("Insufficient funds");
+        }
     }
 }
